d38addccd196e51ecdfe903a760c2e1055a2b97b
rakeshkhanna <rakesh.khanna@live.com>
2024-06-06 16:24:51 +0000

    Data Ingestion

---
+++ src/components/DataIngestion.py
@@ -0,0 +1,46 @@
+import os
+import sys
+from src.Exception import CustomException
+from src.logger import logging
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+#Create Training Data class
+@dataclass
+class DataIngestionConfig:
+    #train data path
+    train_data_path = os.path.join('artifacts','train.csv')
+    #Test Data Path
+    test_data_path = os.path.join('artifacts','test.csv')
+    #Raw Data Path
+    raw_data_path = os.path.join('artifacts','raw.csv')
+
+
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config = DataIngestionConfig()
+    
+    def InitiateDataIngestion_csv(self,file_path:str):
+        logging.info("Starting data Ingestion")
+        try:
+            df = pd.read_csv(file_path)
+            logging.info(f"Created the dataframe from csv at {file_path}")
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
+            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
+            logging.info(f"Raw Data saved at {self.ingestion_config.raw_data_path}")
+            train_set,test_set = train_test_split(df,test_size=0.2, random_state=42)
+            df.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+            logging.info(f"Train Data saved at {self.ingestion_config.train_data_path}")
+            df.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+            logging.info(f"Test Data saved at {self.ingestion_config.test_data_path}")
+            return(
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+        except Exception as e:
+            raise CustomException(e,sys)
+
+if __name__=="__main__":
+    obj = DataIngestion()
+    obj.InitiateDataIngestion_csv('./src/notebooks/data.csv')
